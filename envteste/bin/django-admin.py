6b893e2931a984c0b629503381d3f52acca39051
Bruno <bruno.suporte@gmail.com>
2016-09-04 00:24:12 -0300

    meu codigo teste em Django

---
+++ envteste/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/brunoazevedo/Dropbox/Repository_Git/outros_projetos/android/AmbientePython2/envteste/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
